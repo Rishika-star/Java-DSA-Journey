a56ffab6d635470bc5cdb83126c292feb7685469
Rishika-star <ranjanrishika887@gmail.com>
2026-09-08 11:45:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Logical Operators/logical-operators.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String booleanOperations(boolean a, boolean b) {
+        // Code here
+        //not condition no loop
+        //bs ek print statement me write this
+        return (a&&b)+" "+(a||b)+" "+(!a);
+    }
+}
