0bc20e0a68cd147f29e67d6de949b9a6d73cd9da
Rishika-star <ranjanrishika887@gmail.com>
2026-09-12 23:43:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+       // for(int i=0;i<n;i++){
+            if(n%2==0){
+               // System.out.println("You");condn ulti likh di thi
+               System.out.println("Friend");
+               
+            }
+            else{
+                //System.out.println("Friend");condn ulti likh di thi
+                System.out.println("You");
+            }
+      //  } for ki need nhi h warna multiple times printing hogi
+
+        sc.close();
+    }
+}
