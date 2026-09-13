0c8b257cb48d1f3c98c99540206bdeba0689240a
Rishika-star <ranjanrishika887@gmail.com>
2026-09-13 23:57:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/nextLine and next/next-line-and-next.java
@@ -0,0 +1,11 @@
+class Solution {
+    static void printInput(int n, String s) {
+        // code here
+       /* Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        s=sc.nextLine();*/
+        System.out.println(n);
+        System.out.println(s);
+        
+    }
+}
